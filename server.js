c23f89ff51a0b09580b529f447f87279e82e3a05
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-03-15 16:22:20 -0400

    funcitoning API for GET and POST

--- server.js
+++ server.js
@@ -1,7 +1,37 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+/*-----------------------------------------------------------------------
+- parse incoming string or array data && parse incoming JSON data
+-----------------------------------------------------------------------*/
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const { animals } = require('./data/animals');
+
+/*-----------------------------------------------------------------------
+-                       VALIDATE ANIMALS JSON FILE
+-----------------------------------------------------------------------*/
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
 
 /*-----------------------------------------------------------------------
 -                       Filter Parameters
@@ -49,6 +79,17 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
+}
+
 /*-----------------------------------------------------------------------
 -                       GET DATA (ROUTES)
 -----------------------------------------------------------------------*/
@@ -78,6 +119,23 @@ app.get('/api/animals/:id', (req, res) => {
   } else {
     res.sendStatus(404);
   }
+});
+
+/*-----------------------------------------------------------------------
+-                       PSOT DATA 
+-----------------------------------------------------------------------*/
+
+app.post('/api/animals', (req, res) => {
+ // set id based on what the next index of the array will be
+ req.body.id = animals.length.toString();
+
+ // if any data in req.body is incorrect, send 400 error back
+ if (!validateAnimal(req.body)) {
+  res.status(400).send('The animal is not properly formatted.');
+} else {
+  const animal = createNewAnimal(req.body, animals);
+  res.json(animal);
+}
 });
 
 /*-----------------------------------------------------------------------
